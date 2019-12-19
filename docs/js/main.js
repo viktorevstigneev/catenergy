db40ab08f830271dae687b0337173cfa04197724
viktorevstigneev <viktor_evstigneev14@bk.ru>
2019-12-19 11:29:58 +0300

    fixed js

--- docs/js/main.js
+++ docs/js/main.js
@@ -4,17 +4,18 @@ document.addEventListener('DOMContentLoaded',() =>{
     'use strict';
    const  headerList =document.getElementById('header__list');
    const closeMenu = document.getElementById('close');
+   let  imgCLOP = document.getElementById('imgCO');
     let a=1;
 
 
    closeMenu.addEventListener('click',  () =>{
     if(a==0){  headerList.style.display='block';
-    
+    imgCLOP.src='../img/close.png';
        a=1;
     }
     else {
+       imgCLOP.src='../img/open.png';
         headerList.style.display='none';
-      
        a=0;
     }
     
