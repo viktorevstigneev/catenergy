23fa98884ca95ee67492b1f56afd112fca456c85
viktorevstigneev <viktor_evstigneev14@bk.ru>
2019-12-23 23:43:21 +0300

    chnge all after review

--- www/js/menu.js
+++ www/js/menu.js
@@ -1,21 +1,18 @@
-
-
+'use strict';
 document.addEventListener('DOMContentLoaded',() =>{
-    'use strict';
    const  headerList =document.getElementById('header__list');
    const closeMenu = document.getElementById('close');
-   let  imgCLOP = document.getElementById('imgCO');
+   const  imgCLOP = document.getElementById('imgCO');
     let a=0;
-
-
    closeMenu.addEventListener('click',  () =>{
-    if(a==0){  headerList.style.display='block';
-    imgCLOP.src='img/close.png';
+    if(a==0){  
+       headerList.style.display='block';
+       imgCLOP.src='img/close.png';
        a=1;
     }
     else {
-       imgCLOP.src='img/open.png';
-        headerList.style.display='none';
+      imgCLOP.src='img/open.png';
+       headerList.style.display='none';
        a=0;
     }
     
